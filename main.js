6efbc41a3dc1c4d7b4ca9b624e035da26ca37759
Jaume Villarreal <jaume@MacBook-Air-2.local>
2019-07-09 15:21:10 +0200

    Add innerHTML alert

--- main.js
+++ main.js
@@ -5,5 +5,8 @@ var inputs = document.getElementsByTagName("input");
 var input = inputs[0];
 
 button.onclick = function(){
-    alert("Hello " + input.value + "!!" );
+    var headers = document.getElementsByTagName('h2');
+    var text_box = headers[0];
+    text_box.innerHTML = "Hello " +input.value + "!";
+    input.value='';
 };
